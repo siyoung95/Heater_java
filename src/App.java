340d4056ccf221a0f3f0e8529046ec9c7eee9f5c
siyoung95 <tldud456@naver.com>
2022-01-18 23:37:42 +0900

    최초커밋

---
+++ src/App.java
@@ -0,0 +1,113 @@
+import java.util.Scanner;
+import java.lang.Thread;
+
+public class App {
+
+	public static void main(String[] args) {
+		Heater x = new Heater();
+		
+
+		////히터 강도를 설정한다
+		//1.히터 강도 메뉴
+		System.out.println("히터 강도를 선택하시오");
+		System.out.println("1.히터 (약) 2.히터 (강) 3.히터+온풍 (약) 4.히터+온풍 (강)");
+		//2.메뉴선택 (스캐너로 입력받음)
+		Scanner scan = new Scanner(System.in);
+		int input1 = scan.nextInt();
+		//3.각 메뉴에 맞게 작동 (if문 사용이유: 여러 선택지 중 한개를 선택하려고, while문 사용이유: 선택지 중 하나를 선택할 때까지 반복하려고)
+		while(true) {
+			if(input1 == 1) {
+				x.heat = "@ @ @ @";
+				x.Object();
+				break;
+			} else if (input1 == 2) {
+				x.heat = "@@@@@@@";
+				x.Object();
+				break;
+			} else if (input1 == 3) {
+				x.heat = "@@@@@@@";
+				x.winds = "~ ~ ~";
+				x.Object();
+				break;
+			} else if (input1 == 4) {
+				x.heat = "@@@@@@@";
+				x.winds = "~~~~~";
+				x.Object();
+				break;
+			} else {
+				System.out.println("없는 메뉴입니다. 다시 선택해 주세요.");
+			}
+		}
+		
+		
+		
+		////히터 작동시간을 설정한다
+		//1.히터 작동시간 메뉴
+		System.out.println("히터 작동시간을 선택하시오");
+		System.out.println("1.연속 2.30분 3.60분 4.90분 5.150분 6.180분");
+		//2.메뉴선택 
+		int input2 = scan.nextInt();
+		//3.각 메뉴에 맞게 작동 
+		while(true) {
+			if(input2 == 1) {
+				x.time = 0;
+				x.Object();
+				break;
+			} else if(input2 == 2) {
+				x.time = 30;
+				for(int i=0; i<=x.time; i++) {
+					try {
+						x.Object();
+						x.time--;
+						Thread.sleep(1000);						
+					} catch(InterruptedException e) { }
+				}
+				break;
+			} else if(input2 == 3) {
+				x.time = 60;
+				for(int i=0; i<=x.time; i++) {
+					try {
+						x.Object();
+						x.time--;
+						Thread.sleep(1000);						
+					} catch(InterruptedException e) { }
+				}
+				break;
+			} else if(input2 == 4) {
+				x.time = 90;
+				for(int i=0; i<=x.time; i++) {
+					try {
+						x.Object();
+						x.time--;
+						Thread.sleep(1000);						
+					} catch(InterruptedException e) { }
+				}
+				break;
+			} else if(input2 == 5) {
+				x.time = 150;
+				for(int i=0; i<=x.time; i++) {
+					try {
+						x.Object();
+						x.time--;
+						Thread.sleep(1000);						
+					} catch(InterruptedException e) { }
+				}
+				break;
+			} else if(input2 == 6) {
+				x.time = 180;
+				for(int i=0; i<=x.time; i++) {
+					try {
+						x.Object();
+						x.time--;
+						Thread.sleep(1000);						
+					} catch(InterruptedException e) { }
+				}
+				break;
+			} else {
+				System.out.println("없는 메뉴입니다. 다시 선택해 주세요.");
+			}
+		}
+		scan.close();
+	}
+
+}
---
+++ src/Heater.java
@@ -0,0 +1,28 @@
+
+public class Heater {
+	boolean power;
+	String heat;
+	String winds = "";
+	int time = 0;
+
+	void Object() {
+		System.out.println(" ////////");
+		System.out.println("//ㅡㅡㅡㅡㅡ// " + winds);
+		System.out.println("//ㅡㅡㅡㅡㅡ// " + winds);
+		System.out.println("//ㅡㅡㅡㅡㅡ// " + winds);
+		System.out.println("//ㅡㅡㅡㅡㅡ// " + winds);
+		System.out.println("//@@@@@@@//");
+		System.out.println("//@@@@@@@//");
+		System.out.println("//@@@@@@@//");
+		System.out.println("//@@@@@@@//");
+		System.out.println("//////////");
+		System.out.println("   ///    ");
+		System.out.println("   ///    ");
+		System.out.println("   ///    ");
+		System.out.println("//////////");
+		System.out.println("////" + time + "////");
+		System.out.println("//////////");
+		System.out.println();
+
+	}
+}
